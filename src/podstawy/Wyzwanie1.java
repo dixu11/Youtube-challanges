52b6e65c28dab542f1571e238eedc842b338e89a
Dixu <you.are.imba@gmail.com>
2020-06-24 23:09:07 +0200

    add challange 1

---
+++ src/podstawy/Wyzwanie1.java
@@ -0,0 +1,18 @@
+package podstawy;
+
+public class Wyzwanie1 {
+
+   // Wyzwanie pierwsze: "Proste drukowanie tekstu"
+
+   // Rozbijmy je na małe części i do roboty! ☺
+
+
+    public static void main(String[] args) {
+        System.out.println("Wyzwanie pierwsze: \"Prote drukowanie tekstu\"");
+        System.out.println();
+        System.out.println("Rozbijmy je na małe części i do roboty! \u263A");
+
+//        System.out.println("Wyzwanie pierwsze: Proste drukowanie tekstu\n\nRozbijmy je na małe części i do roboty!");
+    }
+
+}
